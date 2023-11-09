62c4755100fcdc5833b18f26ab2772d33935a58a
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2023-11-09 21:14:45 +0700

    Basic

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import fs from 'fs';
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [
+		preact(),
+	],
+	build: {
+		rollupOptions: {
+		  input: {
+			user: resolve(__dirname, 'src/index.tsx'),
+		  },
+
+		  output: {
+			entryFileNames: `gmaps-coords.user.js`,
+		  }
+		},
+	},
+	esbuild: {
+		banner: fs.readFileSync('userScriptHeader.txt', 'utf8'),
+	}
 });
